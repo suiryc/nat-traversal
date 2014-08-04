4861cd98c650c98f91dd85a66d052d0aaadfdd5e
Julien Coloos <julien.coloos@gmail.com>
2014-08-04 21:51:54 +0200

    Automatically copy pom upon compile

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,22 @@ import Keys._
 
 object NatTraversalBuild extends Build {
 
+  lazy val base = file(".").getCanonicalFile
+
   lazy val copyPom = TaskKey[Unit]("copy-pom")
 
-  def copyPomTask(base: File) = copyPom <<= makePom map { pom =>
-    IO.copyFile(pom, base / "pom.xml")
+  val copyPomTask = copyPom <<= (makePom, streams) map { (pom, s) =>
+    val dest = base / "pom.xml"
+    s.log.info(s"Copy pom: $dest")
+    IO.copyFile(pom, dest)
   }
 
-  lazy val base = file(".").getCanonicalFile
+  val extCompile = compile <<= (compile in Compile) dependsOn(copyPom)
 
   lazy val root = Project(
     id = "nat-traversal",
     base = base,
-    settings = Defaults.defaultSettings ++ Seq(copyPomTask(base))
+    settings = Defaults.defaultSettings ++ Seq(copyPomTask, extCompile)
   )
 }
 
